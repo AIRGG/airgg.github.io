2b073fe66f06e98a2229e60caf8d675f15c986f4
AIRGG <>
2021-08-12 05:27:53 +0700

    web profile atas

---
+++ _nuxt/static/1628720606/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcoba","\u002Findexlama","\u002Ftugasiwp2","\u002FindexXX","\u002Ftesting"]})
---
+++ _nuxt/static/1628720606/tugasiwp2/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/tugasiwp2", {data:[{}],fetch:{},mutations:void 0});
